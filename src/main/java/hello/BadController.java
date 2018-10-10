8c328bb4a6d7478e096f515d5dd15db06e740277
Ziquan Miao <ziquan.miao@datadoghq.com>
2018-10-10 15:47:34 -0400

    add statsd and parent level tagging

--- src/main/java/hello/BadController.java
+++ src/main/java/hello/BadController.java
@@ -1,5 +1,11 @@
 package hello;
 
+import java.util.concurrent.ThreadLocalRandom;
+
+import io.opentracing.Tracer;
+import io.opentracing.util.GlobalTracer;
+import datadog.trace.api.interceptor.MutableSpan;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +14,13 @@ public class BadController {
 
     @RequestMapping("/bad")
     public String index() {
+    	final Tracer tracer = GlobalTracer.get();
+    	if (tracer != null && tracer.activeSpan() != null) {
+			((MutableSpan)tracer.activeSpan()).getRootSpan().setTag("customer", ThreadLocalRandom.current().nextInt());
+	    }
     	try 
 		{
+
 		    Thread.sleep(4);
 		} 
 		catch(InterruptedException e)
--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,13 +1,34 @@
 package hello;
 
+import java.util.concurrent.ThreadLocalRandom;
+
+import io.opentracing.Tracer;
+import io.opentracing.util.GlobalTracer;
+import datadog.trace.api.interceptor.MutableSpan;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.timgroup.statsd.ServiceCheck;
+import com.timgroup.statsd.StatsDClient;
+import com.timgroup.statsd.NonBlockingStatsDClient;
+
 @RestController
 public class HelloController {
-
+	private static final StatsDClient statsd = new NonBlockingStatsDClient(
+		"my.prefix",                          /* prefix to any stats; may be null or empty string */
+		System.getenv("DD_AGENT_SERVICE_HOST"),                        /* common case: localhost */
+		Integer.parseInt(System.getenv("DD_AGENT_STATSD_PORT")),                                 /* port */
+		new String[] {"test:tag"}            /* Datadog extension: Constant tags, always applied */
+	);
+	
     @RequestMapping("/")
     public String index() {
+    	statsd.incrementCounter("foo");
+
+    	final Tracer tracer = GlobalTracer.get();
+    	if (tracer != null && tracer.activeSpan() != null) {
+			((MutableSpan)tracer.activeSpan()).getRootSpan().setTag("customer", ThreadLocalRandom.current().nextInt());
+	    }
         return "Greetings from Spring Boot!";
     }
 
